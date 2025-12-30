624b9afd310e60747269f52283374d7ddfc21a57
Bikram Paul <bikrampaul7044@gmail.com>
2025-12-30 19:10:41 +0530

    made database connections between inngest clerk and mongoDb and made some changes for deployment

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,27 +1,34 @@
+import express from "express";
+import { Env } from "./src/lib/Env.js";
+import { connectDb } from "./src/lib/Db.js";
+import cors from "cors";
 
-import express from "express"
-import { Env } from "./src/lib/Env.js"
-import { connectDb } from "./src/lib/Db.js"
+const app = express();
 
-const app=express()
+app.use(
+  cors({
+    origin: Env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).json({
-        msg:"Success"
-    })
-})
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "Success",
+  });
+});
 
-let port=Env.Port
+let port = Env.Port;
 
-const server= async () =>{
-    try{
-        await connectDb();
-        app.listen(port,()=>{
-            console.log(`App is listening at post ${port}`)
-        })
-    }
-    catch{
-        console.error("there is an error in server")
-    }
-}
-server()
+const server = async () => {
+  try {
+    await connectDb();
+    app.listen(port, () => {
+      console.log(`App is listening at post ${port}`);
+    });
+  } catch {
+    console.error("there is an error in server");
+  }
+};
+server();
---
+++ Backend/src/models/User.js
@@ -0,0 +1,28 @@
+import mongoose, { Model }  from "mongoose";
+
+const userSchema=new mongoose.Schema({
+name:{
+    type:String,
+    required:true,
+    
+},
+email:{
+    type:String,
+    required:true,
+    unique:true
+},
+profileImage:{
+    type:String
+}
+,
+clerkId:{
+    type:String,
+    required:true,
+    unique:true
+}
+},{
+    timestamps:true
+})
+
+const User=mongoose.model("User",userSchema)
+export default User

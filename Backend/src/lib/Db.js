4d557cde974ed813784e829e5a4f099fdb4f6dd7
Bikram Paul <bikrampaul7044@gmail.com>
2025-12-30 12:39:01 +0530

    validation check added for Db_url

--- Backend/src/lib/Db.js
+++ Backend/src/lib/Db.js
@@ -3,6 +3,7 @@ import { Env } from "./Env.js";
 
 export const connectDb = async () =>{
     try{
+    if(!Env.Db_Url) throw new error ("Databse Url not found")
      const conn =   await mongoose.connect(Env.Db_Url)
      console.log(conn.connection.host)
         console.log("Database Connected")

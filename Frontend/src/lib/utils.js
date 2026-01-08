f7b1449b3bbe0cd9081a2d32e2fcdd337a8b4624
Bikram Paul <bikrampaul7044@gmail.com>
2026-01-08 16:59:00 +0530

    Created Problems page and added some problems and added Ui

---
+++ Frontend/src/lib/utils.js
@@ -0,0 +1,13 @@
+export const problemDifficulty=(difficulty)=>{
+    switch(difficulty.toLowerCase()){
+        case "easy":
+        return "badge-success"
+        case "medium":
+        return "badge-warning"
+        case"hard":
+        return "badge-error"
+        default:
+        return "badge-neutral"
+        
+    }
+}

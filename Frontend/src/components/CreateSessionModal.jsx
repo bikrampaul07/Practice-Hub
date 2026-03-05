4afcbffc56ec71f5900fd60c406ce2ba43508e0c
bikrampaul07 <ORG_EMAIL@example.com>
2026-03-05 12:17:37 +0530

    small changes in CreateSessionModal

--- Frontend/src/components/CreateSessionModal.jsx
+++ Frontend/src/components/CreateSessionModal.jsx
@@ -1,5 +1,5 @@
 import { Code2Icon, LoaderIcon, PlusIcon } from "lucide-react";
-import { PROBLEMS } from "../data/problems";
+import { PROBLEMS } from "../Data/problems.js";
 
 function CreateSessionModal({
   isOpen,
